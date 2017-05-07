3d52eaae41b553b1a0b78feae7c9645292a9b71e
Stricted <info@stricted.net>
2017-05-07 08:08:05 +0200

    update to G903FXXU1BQD1

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY:20230144:6c4609900c26f3577aec4e95a076eed84fb0884a; then
-  applypatch EMMC:/dev/block/platform/13540000.dwmmc0/by-name/BOOT:18857984:94d6ed9d18abe10ac712dcca3c51f802d38da402 EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY 6c4609900c26f3577aec4e95a076eed84fb0884a 20230144 94d6ed9d18abe10ac712dcca3c51f802d38da402:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY:20230144:880bec22683548e908136f764c0e8b2db47fca55; then
+  applypatch EMMC:/dev/block/platform/13540000.dwmmc0/by-name/BOOT:18857984:8ef06fe3ec40c3cb6d9ac3dc0b716e56008aa2a5 EMMC:/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY 880bec22683548e908136f764c0e8b2db47fca55 20230144 8ef06fe3ec40c3cb6d9ac3dc0b716e56008aa2a5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
